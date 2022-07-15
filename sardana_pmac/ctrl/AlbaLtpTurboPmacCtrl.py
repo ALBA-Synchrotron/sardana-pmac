11440fa46cbcd5fb8c76dbef51ee847c81eafd49
Roberto Javier Homs Puron <rhoms@cells.es>
2022-07-15 10:57:24 +0200

    Apply 2to3 and autopep8 to AlbaLtpTurboPmacCtrl.py

--- sardana_pmac/ctrl/AlbaLtpTurboPmacCtrl.py
+++ sardana_pmac/ctrl/AlbaLtpTurboPmacCtrl.py
@@ -2,52 +2,51 @@
 
 #############################################################################
 ##
-## file :    PmacLTPCtrl.py
+# file :    PmacLTPCtrl.py
 ##
-## description : 
+# description :
 ##
-## project :    miscellaneous/PoolControllers/MotorControllers
+# project :    miscellaneous/PoolControllers/MotorControllers
 ##
-## developers history: zreszela
+# developers history: zreszela
 ##
-## copyleft :    Cells / Alba Synchrotron
-##               Bellaterra
-##               Spain
+# copyleft :    Cells / Alba Synchrotron
+# Bellaterra
+# Spain
 ##
 #############################################################################
 ##
-## This file is part of Sardana.
+# This file is part of Sardana.
 ##
-## This is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 3 of the License, or
-## (at your option) any later version.
+# This is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
 ##
-## This software is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 ##
-## You should have received a copy of the GNU General Public License
-## along with this program; if not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
 ###########################################################################
 
 import PyTango
-from TurboPmacCtrl import TurboPmacController
-from sardana import pool
-from sardana.pool import PoolUtil
-from sardana.pool.controller import MotorController
+from .TurboPmacCtrl import TurboPmacController
+
 
 class LtpTurboPmacController(TurboPmacController):
-    """This class is the Tango Sardana motor controller for the Turbo Pmac motor controller device in LTP."""
-    
+    """This class is the Tango Sardana motor controller for the Turbo Pmac
+        motor controller device in LTP."""
+
     MaxDevice = 2
 
     axis_attributes = dict(TurboPmacController.axis_attributes)
-    axis_attributes['FeedbackMode'] = {'Type':int,
-                                       'Access':'ReadOnly'}
+    axis_attributes['FeedbackMode'] = {'Type': int,
+                                       'Access': 'ReadOnly'}
 
-    def __init__(self,inst,props,*args,**kwargs):
+    def __init__(self, inst, props, *args, **kwargs):
         TurboPmacController.__init__(self, inst, props, *args, **kwargs)
         self.superklass = TurboPmacController
 
@@ -65,15 +64,24 @@ class LtpTurboPmacController(TurboPmacController):
                 elif mode == "$3503":
                     return 2
                 else:
-                    self._log.error("While getting feedback mode TurboPmac returned some inconsistent value, please report it to controls division.")
+                    self._log.error("While getting feedback mode TurboPmac "
+                                    "returned some inconsistent value, "
+                                    "please report it to controls division.")
                     PyTango.Except.throw_exception("Value error",
-                                                   "TurboPmac returned some inconsistent value, please report it to controls division.",
-                                                   "LtpTurboPmacController.GetExtraAttribute()")
+                                                   "TurboPmac returned some "
+                                                   "inconsistent value, please"
+                                                   " report it to controls"
+                                                   " division.",
+                                                   "LtpTurboPmacController"
+                                                   ".GetExtraAttribute()")
             if axis == 2:
-                self._log.warning("Various feedback mode feature is reserved only for top axis.")
-                PyTango.Except.throw_exception("Value error",
-                                           "Axis nr 2 does not support various feedback mode.",
-                                           "LtpTurboPmacController.GetExtraAttribute()")
+                self._log.warning(
+                    "Various feedback mode feature is reserved only for "
+                    "top axis.")
+                PyTango.Except.throw_exception(
+                    "Value error",
+                    "Axis nr 2 does not support various feedback mode.",
+                    "LtpTurboPmacController.GetExtraAttribute()")
         else:
             return self.superklass.GetExtraAttributePar(self, axis, name)
 
@@ -86,19 +94,26 @@ class LtpTurboPmacController(TurboPmacController):
         if name == "FeedbackMode":
             if axis == 1:
                 if value == 1:
-                    self.pmacEth.command_inout("OnlineCmd","I103=$3501")
+                    self.pmacEth.command_inout("OnlineCmd", "I103=$3501")
                 elif value == 2:
-                    self.pmacEth.command_inout("OnlineCmd","I103=$3503")
+                    self.pmacEth.command_inout("OnlineCmd", "I103=$3503")
                 else:
-                    self._log.warning("Feedback supports only two modes: use 1 for single feedback mode or 2 for dual feedback mode.")
-                    PyTango.Except.throw_exception("Value error",
-                                           "Wrong value, use 1 for single feedback mode or 2 for dual feedback mode.",
-                                           "PmacLTPCtrl.SetExtraAttribute()")
+                    self._log.warning(
+                        "Feedback supports only two modes: use 1 for single "
+                        "feedback mode or 2 for dual feedback mode.")
+                    PyTango.Except.throw_exception(
+                        "Value error",
+                        "Wrong value, use 1 for single feedback mode or 2"
+                        " for dual feedback mode.",
+                        "PmacLTPCtrl.SetExtraAttribute()")
             else:
-                self._log.warning("Various feedback mode feature is reserved only for top axis.")
-                PyTango.Except.throw_exception("Value error",
-                                           "Axis nr 2 does not support various feedback mode.",
-                                           "PmacLTPCtrl.SetExtraAttribute()")
+                self._log.warning(
+                    "Various feedback mode feature is reserved only for"
+                    " top axis.")
+                PyTango.Except.throw_exception(
+                    "Value error",
+                    "Axis nr 2 does not support various feedback mode.",
+                    "PmacLTPCtrl.SetExtraAttribute()")
 
                 return
         else:
