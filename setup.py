77bfcffdda3978e0d8f43775d779566c5edebbe8
Roberto Javier Homs Puron <rhoms@cells.es>
2022-08-17 12:31:10 +0200

    Bump version 0.2.1 to 0.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sardana-pmac",
-    version="0.2.1",
+    version="0.2.2",
     description="Pmac Sardana plugins (controllers, macros, tools)",
     author="ALBA controls team",
     author_email="controls@cells.es",
